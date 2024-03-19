3f4d2c33f91d87ac61c8c38b2766517c97c0ccdc
[ComaN10] <[carlosmanuel2003@gmail.com]>
2024-03-19 00:31:00 +0000

    Update misc.xml, Main.java, MainForm.java, and 1 more file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,40 +2,48 @@ import javax.swing.*;
 import java.awt.image.BufferedImage;
 import Network.Client.*;
 import Network.Server.*;
-import Utils.Image.ImageReader;
-import Utils.Image.ImageTransformer;
+import UI.MainForm;
+import Utils.Image.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Server server = new Server(8888);
-        server.start();
-        BufferedImage sampleImage = ImageReader.readImage("sample.png");
-        // Java Swing stuff
-        JFrame frame = new JFrame("pa-distributed-img");
-        frame.setSize(400, 400);
-        JPanel panel = new JPanel();
-        ImageIcon icon = new ImageIcon(sampleImage);
-        JLabel label = new JLabel(icon);
-        panel.add(label);
-
-        JButton button = new JButton();
-        button.setText("Remove red");
-        panel.add(button);
-
-        button.addActionListener(e -> {
-            Client client = new Client("Client A");
-            Request request = new Request("greeting", "Hello, Server!", sampleImage);
-            Response response = client.sendRequestAndReceiveResponse("localhost", 8888, request);
-            icon.setImage(ImageTransformer.createImageFromBytes(response.getImageSection()));
-            panel.repaint();
-        });
-
-        frame.add(panel);
+    public static void main ( String[] args ) {
+        Server server = new Server ( 8888 );
+        server.start ( );
+        JFrame frame = new JFrame("Main Form");
+        MainForm mainForm = new MainForm();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(mainForm);
         frame.pack();
         frame.setVisible(true);
+
+//        BufferedImage sampleImage = ImageReader.readImage("sample.png");
+//        //Java Swing stuff
+//        JFrame frame = new JFrame ( "pa-distributed-img" );
+//        frame.setSize ( 400 , 400 );
+//        JPanel panel = new JPanel ( );
+//        ImageIcon icon = new ImageIcon ( sampleImage );
+//        JLabel label = new JLabel ( icon );
+//        panel.add ( label );
+//
+//        JButton button = new JButton ( );
+//        button.setText ( "Remove red" );
+//        panel.add ( button );
+//
+//
+//        button.addActionListener( e -> {
+//            Client client = new Client ( "Client A" );
+//            Request request = new Request ( "greeting" , "Hello, Server!",sampleImage );
+//            Response response = client.sendRequestAndReceiveResponse ( "localhost" , 8888 , request );
+//            icon.setImage(ImageTransformer.createImageFromBytes(response.getImageSection()));
+//            panel.repaint();
+//        });
+//
+//        frame.add ( panel );
+//        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+//        frame.pack ( );
+//        frame.setVisible ( true );
+
 
     }
 
