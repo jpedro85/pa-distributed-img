05f104307000960ae0f5b4ece2ee664e6a49b40a
KingRichardpt45 <ricfrancvi@gmail.com>
2024-03-20 14:34:20 +0000

    feat: Start of ClientHandler

---
+++ src/main/java/Network/Client/ClientHandler.java
@@ -0,0 +1,30 @@
+package Network.Client;
+
+import Utils.Events.Event;
+import Utils.Observer.Observer;
+import Utils.Observer.Subject;
+
+import java.util.ArrayList;
+
+
+public class ClientHandler implements Observer {
+
+    private final ArrayList<MasterClient> clients;
+
+    public void addClient(){
+
+    }
+
+    private void cancelClient(){
+
+    }
+
+    public void removeClient(){
+
+    }
+    @Override
+    public void update(Subject subject, Event event) {
+
+    }
+
+}
